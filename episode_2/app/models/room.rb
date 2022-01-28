6f01c9adff5d768cf412bd8675b7af1808adb57f
Dean DeHart <DeHartDean@gmail.com>
2022-01-28 01:12:14 -0500

    Revert "Begin episode 2"
    This reverts commit 1b6334d86883968647173ff9422e77352fd14c9a.

--- episode_2/app/models/room.rb
+++
@@ -1,6 +0,0 @@
-class Room < ApplicationRecord
-  validates_uniqueness_of :name
-  scope :public_rooms, -> { where(is_private: false) }
-  after_create_commit { broadcast_append_to 'rooms' }
-  has_many :messages
-end
