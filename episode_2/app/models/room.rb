428a75a34a5116bca16a1fd510d8823e9704b94a
Dean DeHart <DeHartDean@gmail.com>
2022-01-28 05:41:47 -0500

    Add episode 2

---
+++ episode_2/app/models/room.rb
@@ -0,0 +1,19 @@
+class Room < ApplicationRecord
+  validates_uniqueness_of :name
+  scope :public_rooms, -> { where(is_private: false) }
+  after_create_commit { broadcast_if_public }
+  has_many :messages
+  has_many :participants, dependent: :destroy
+
+  def broadcast_if_public
+    broadcast_append_to 'rooms' unless is_private
+  end
+
+  def self.create_private_room(users, room_name)
+    single_room = Room.create(name: room_name, is_private: true)
+    users.each do |user|
+      Participant.create(user_id: user.id, room_id: single_room.id)
+    end
+    single_room
+  end
+end
---
+++ episode_2/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  scope :all_except, ->(user) { where.not(id: user) }
+  after_create_commit { broadcast_append_to 'users' }
+  has_many :messages
+end
