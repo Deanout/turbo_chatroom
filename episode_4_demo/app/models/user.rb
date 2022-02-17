3b16f07bd8e9f3f06677be97a3a57c810feea2df
Dean DeHart <DeHartDean@gmail.com>
2022-02-17 14:39:17 -0500

    Init ep4 commit

---
+++ episode_4_demo/app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  scope :all_except, ->(user) { where.not(id: user) }
+  after_create_commit { broadcast_append_to 'users' }
+  has_many :messages
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: %i[create update]
+
+  def avatar_thumbnail
+    avatar.variant(resize_to_limit: [150, 150]).processed
+  end
+
+  def chat_avatar
+    avatar.variant(resize_to_limit: [50, 50]).processed
+  end
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_profile.jpg')),
+      filename: 'default_profile.jpg',
+      content_type: 'image/jpg'
+    )
+  end
+end
