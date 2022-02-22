057187d49ff785c8dc6e50bcc71f6b7a895972c2
Dean DeHart <DeHartDean@gmail.com>
2022-02-22 03:39:01 -0500

    Cleanup storage

---
+++ episode_6_demo/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @users = User.all_except(current_user)
+
+    @room = Room.new
+    @rooms = Room.public_rooms
+    @room_name = get_name(@user, current_user)
+    @single_room = Room.where(name: @room_name).first || Room.create_private_room([@user, current_user], @room_name)
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+    render 'rooms/index'
+  end
+
+  private
+
+  def get_name(user1, user2)
+    user = [user1, user2].sort
+    "private_#{user[0].id}_#{user[1].id}"
+  end
+end
