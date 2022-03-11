d2cdf4b1e5a5555a3ad5fba21c509b8d4a38e510
Dean DeHart <DeHartDean@gmail.com>
2022-03-11 04:09:52 -0500

    Episode 8 commit

---
+++ episode_8/app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  initialize() {
+    this.resetScrollWithoutThreshold(messages);
+  }
+  /** On start */
+  connect() {
+    console.log("Connected scroll");
+    const messages = document.getElementById("messages");
+    messages.addEventListener("DOMNodeInserted", this.resetScroll);
+  }
+  /** On stop */
+  disconnect() {
+    console.log("Disconnected");
+  }
+  /** Custom function */
+  resetScroll() {
+    const bottomOfScroll = messages.scrollHeight - messages.clientHeight;
+    const upperScrollThreshold = bottomOfScroll - 500;
+    // Scroll down if we're not within the threshold
+    if (messages.scrollTop > upperScrollThreshold) {
+      messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+    }
+  }
+  resetScrollWithoutThreshold(messages) {
+    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+  }
+}
---
+++ episode_8/episode_7/app/javascript/channels/appearance_channel.js
@@ -0,0 +1,74 @@
+import consumer from "channels/consumer";
+
+let resetFunc;
+let timer = 0;
+
+consumer.subscriptions.create("AppearanceChannel", {
+  initialized() {},
+  connected() {
+    // Called when the subscription is ready for use on the server
+    console.log("Connected");
+    resetFunc = () => this.resetTimer(this.uninstall);
+    this.install();
+    window.addEventListener("turbo:load", () => this.resetTimer());
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+    console.log("Connected");
+    this.uninstall();
+  },
+  rejected() {
+    console.log("Rejected");
+    this.uninstall();
+  },
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+  },
+  online() {
+    console.log("online");
+    this.perform("online");
+  },
+  away() {
+    console.log("away");
+    this.perform("away");
+  },
+  offline() {
+    console.log("offline");
+    this.perform("offline");
+  },
+  uninstall() {
+    const shouldRun = document.getElementById("appearance_channel");
+    if (!shouldRun) {
+      clearTimeout(timer);
+      this.perform("offline");
+    }
+  },
+  install() {
+    console.log("Install");
+    window.removeEventListener("load", resetFunc);
+    window.removeEventListener("DOMContentLoaded", resetFunc);
+    window.removeEventListener("click", resetFunc);
+    window.removeEventListener("keydown", resetFunc);
+
+    window.addEventListener("load", resetFunc);
+    window.addEventListener("DOMContentLoaded", resetFunc);
+    window.addEventListener("click", resetFunc);
+    window.addEventListener("keydown", resetFunc);
+    this.resetTimer();
+  },
+  resetTimer() {
+    this.uninstall();
+    const shouldRun = document.getElementById("appearance_channel");
+
+    if (!!shouldRun) {
+      this.online();
+      clearTimeout(timer);
+      const timeInSeconds = 5;
+      const milliseconds = 1000;
+      const timeInMilliseconds = timeInSeconds * milliseconds;
+
+      timer = setTimeout(this.away.bind(this), timeInMilliseconds);
+    }
+  },
+});
