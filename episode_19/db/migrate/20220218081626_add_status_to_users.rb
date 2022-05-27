d110508395cd3d67fa1e2adf6b137bcb108e3232
Dean DeHart <DeHartDean@gmail.com>
2022-05-27 04:59:51 -0400

    E19 commit

---
+++ episode_19/db/migrate/20220218081626_add_status_to_users.rb
@@ -0,0 +1,5 @@
+class AddStatusToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :status, :integer, default: 0
+  end
+end
---
+++ episode_19_demo/config/application.rb
@@ -0,0 +1,25 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Turbochat
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    config.action_view.sanitized_allowed_tags = Loofah::HTML5::SafeList::ALLOWED_ELEMENTS
+    config.action_view.sanitized_allowed_attributes = Loofah::HTML5::SafeList::ALLOWED_ATTRIBUTES
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
