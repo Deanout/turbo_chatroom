1ab723d9520dd3c5ae536da2e3d0476e513c5e09
Dean DeHart <DeHartDean@gmail.com>
2022-06-16 22:08:02 -0400

    Episode 22 init commit

---
+++ episode_21/app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  initialize() {
+    this.resetScrollWithoutThreshold(messages);
+  }
+  /** On start */
+  connect() {
+    console.log("Connected scroll");
+    const messages = document.getElementById("messages");
+    messages.addEventListener("DOMNodeInserted", this.resetScroll);
+    this.resetScrollWithoutThreshold(messages);
+  }
+  /** On stop */
+  disconnect() {
+    console.log("Disconnected");
+  }
+  /** Custom function */
+  resetScroll() {
+    const bottomOfScroll = messages.scrollHeight - messages.clientHeight;
+    const upperScrollThreshold = bottomOfScroll - 500;
+    // Scroll down if we're not within the threshold
+    if (messages.scrollTop > upperScrollThreshold) {
+      messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+    }
+    const audio_tag = document.getElementById("audio-tag");
+    audio_tag.play();
+  }
+  resetScrollWithoutThreshold(messages) {
+    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+  }
+}
